96a7ffdee77e4e98e15739ea7084a5d393255f8a
EKATERINAelnova <katakata45882@gmail.com>
2026-09-12 14:39:12 +0300

    profile_rus

--- assets/preview_profile.py
+++ assets/preview_profile.py
@@ -22,7 +22,7 @@ from utils.profile_card_v2 import (
 
 
 AVATAR_PATH = ASSETS_DIR / "preview_avatar.png"
-OUTPUT_PATH = PROJECT_ROOT / "preview_template_v2.png"
+OUTPUT_PATH = PROJECT_ROOT / "preview_template.png"
 
 
 class PreviewAvatar:
